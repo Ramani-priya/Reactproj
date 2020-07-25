97a2e1c6c68755d5c97e407155d82c656c5cfb81
Ramani-priya <168r1a04k4@cmrec.ac.in>
2020-07-25 18:45:58 +0530

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 /*
 function App() {
   return (
@@ -16,6 +18,12 @@ function App() {
   );
 }*/
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dishes: DISHES
+        };
+    }
     render() {
         return (
             < div className="App" >
@@ -24,7 +32,7 @@ class App extends Component {
                         <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
                     </div>
                 </Navbar>
-               <Menu/>
+                <Menu dishes={this.state.dishes} />
             </div >
         );
     }

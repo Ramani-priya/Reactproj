091e57d84619a1d57112f544f88fac137a0a8edc
Ramani-priya <168r1a04k4@cmrec.ac.in>
2020-08-03 19:34:30 +0530

    Single page app part 1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
+import { BrowserRouter } from 'react-router-dom';
 
-
-
-/*
-function App() {
-  return (
-    <div className="App">
-          <Navbar dark color="primary">
-              <div className="container">
-                  <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
-              </div>
-          </Navbar>
-          <Menu/>
-    </div>
-  );
-}*/
 class App extends Component {
 
     render() {
         return (
-            <div className="App" >
-             <Main/>  
-            </div >
+            <BrowserRouter>
+                <div className="App" >
+                <Main/>  
+                </div >
+            </BrowserRouter>
         );
     }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
 import '../App.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { DISHES } from '../shared/dishes';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+import { COMMENTS } from '../shared/comments';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import DishDetail from './DishDetailComponent';
+import Contact from './ContactComponent';
+//import DishDetail from './DishDetailComponent';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
+            promotions: PROMOTIONS,
+            leaders: LEADERS,
+            comments:COMMENTS,
             selectedDish:null
         };
     }
@@ -17,15 +27,25 @@ class Main extends Component {
         this.setState({selectedDish: dishId});
     }
     render() {
+        const HomePage = () => {
+            return (
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) =>promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
+                />
+               );
+        }
         return (
             <div>
-                <Navbar dark color="primary">
-                        <div className="container">
-                            <NavbarBrand href="/">Ristorante ConFusion</NavbarBrand>
-                        </div>
-                </Navbar>
-                <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId) } />
-                {this.state.selectedDish!=null && <DishDetail dish={this.state.dishes.filter((dish) => this.state.selectedDish === dish.id)[0]} />}
+                <Header />
+                <Switch>
+                    <Route path='/home' component={HomePage}/>
+                    <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/contactus' component={Contact} />
+                    <Redirect to='/home' />
+                </Switch>
+                
+                <Footer/>
             </div>
         );
     }
